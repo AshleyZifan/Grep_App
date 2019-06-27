7ec72e8c71ef41b372c407f5b4b5a9855ee67294
gaozifan <gzf222ash@gmail.com>
2019-06-27 11:38:53 -0400

    removed main

--- grep/src/ca/jrvs/apps/grep/JavaGrepImp.java
+++ grep/src/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -17,6 +17,21 @@ public class JavaGrepImp implements JavaGrep{
     private String regex;
     private String outFile;
 
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("USAGE: regex rootPath outFile");
+        }
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+        try {
+            javaGrepImp.process();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     @Override
     public void process() throws IOException {
         List<String> matchedLines = new ArrayList<String>();
